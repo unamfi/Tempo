71ee9fd815b3e38c4e09e13e22c5d710533cb0d2
Julio César Guzmán <julio.guzman@me.com>
2016-05-07 15:34:00 -0500

    Add folding cell example

--- Angel Hack/ViewController.swift
+++ Angel Hack/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FoldingCell
 
 class ViewController: UIViewController {
 
@@ -19,7 +20,109 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+}
+
+class MainTableViewController: UITableViewController {
+    
+    let kCloseCellHeight: CGFloat = 179
+    let kOpenCellHeight: CGFloat = 488
+    
+    let kRowsCount = 10
+    
+    var cellHeights = [CGFloat]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createCellHeightsArray()
+        self.tableView.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
+    }
+    
+    // MARK: configure
+    func createCellHeightsArray() {
+        for _ in 0...kRowsCount {
+            cellHeights.append(kCloseCellHeight)
+        }
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if cell is FoldingCell {
+            let foldingCell = cell as! FoldingCell
+            foldingCell.backgroundColor = UIColor.clearColor()
+            
+            if cellHeights[indexPath.row] == kCloseCellHeight {
+                foldingCell.selectedAnimation(false, animated: false, completion:nil)
+            } else {
+                foldingCell.selectedAnimation(true, animated: false, completion: nil)
+            }
+        }
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("FoldingCell", forIndexPath: indexPath)
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return cellHeights[indexPath.row]
+    }
+    
+    // MARK: Table vie delegate
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let cell = tableView.cellForRowAtIndexPath(indexPath) as! FoldingCell
+        
+        if cell.isAnimating() {
+            return
+        }
+        
+        var duration = 0.0
+        if cellHeights[indexPath.row] == kCloseCellHeight { // open cell
+            cellHeights[indexPath.row] = kOpenCellHeight
+            cell.selectedAnimation(true, animated: true, completion: nil)
+            duration = 0.5
+        } else {// close cell
+            cellHeights[indexPath.row] = kCloseCellHeight
+            cell.selectedAnimation(false, animated: true, completion: nil)
+            duration = 0.8
+        }
+        
+        UIView.animateWithDuration(duration, delay: 0, options: .CurveEaseOut, animations: { () -> Void in
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            }, completion: nil)
+        
+        
+    }
+}
 
 
+class DemoCell: FoldingCell {
+    
+    override func awakeFromNib() {
+  
+//        foregroundView.layer.cornerRadius = 10
+//        foregroundView.layer.masksToBounds = true
+        super.awakeFromNib()
+        
+       
+    }
+    
+    override func animationDuration(itemIndex:NSInteger, type:AnimationType)-> NSTimeInterval {
+        
+        let durations = [0.26, 0.2, 0.2]
+        return durations[itemIndex]
+    }
+    
 }
 
