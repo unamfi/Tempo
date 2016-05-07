67d525a95ac3d9643ef3dc5c8ab983eeb92710b5
Julio César Guzmán <julio.guzman@me.com>
2016-05-07 13:11:25 -0500

    Initial Commit

---
+++ Angel Hack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Angel Hack
+//
+//  Created by Julio César Guzman on 5/7/16.
+//  Copyright © 2016 Julio Guzman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
