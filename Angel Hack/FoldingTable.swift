bd06ada46200341787f6e8d2b251e31714376ae3
Julio César Guzmán <julio.guzman@me.com>
2016-05-08 05:18:22 -0500

    Separate tableview cells

--- Angel Hack/FoldingTable.swift
+++ Angel Hack/FoldingTable.swift
@@ -52,7 +52,7 @@ class MainTableViewController: UITableViewController {
     // MARK: - Table view data source
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return kRowsCount
+        return 2//kRowsCount
     }
     
     override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
@@ -68,7 +68,7 @@ class MainTableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cellIdentifier = "\(kFoldingCellReuseIdentifier)0"
+        let cellIdentifier = "\(kFoldingCellReuseIdentifier)\(indexPath.row)"
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
         return cell
     }
